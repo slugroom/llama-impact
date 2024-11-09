fa32b692d97d284fb545954e3ce726db4f89d153
sigurdurhaukur <56935611+sigurdurhaukur@users.noreply.github.com>
2024-11-10 00:09:49 +0100

    created the pred script

---
+++ pred.py
@@ -0,0 +1,21 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+class Llama_frisian:
+    def __init__(self):
+        self.tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-1B")
+        self.model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.2-1B")
+
+    def error_correct(self, txt):
+        in_txt = f"### Prediction: {txt} ### Corrected: "
+        model_inputs = self.tokenizer([in_txt], return_tensors="pt")
+        generated_ids = self.model.generate(**model_inputs, max_new_tokens=50)
+
+        return self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+
+
+llama = Llama_frisian()
+
+corrected = llama.error_correct("hello my name is Josh")
+
+print(corrected)
