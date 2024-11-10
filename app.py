93536b234bb54bcd2a21074eff2c9fb586109752
Saker Alabas <salabas@localhost.localdomain>
2024-11-10 20:59:18 +0100

    Added file upload route

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 import flask
+import pred
+
+speechToText = pred.Wave2Vec2_frisian
+llamaFrisian = pred.Llama_frisian
 
 app = flask.Flask(__name__)
 
 @app.route("/")
 def main_page():
     return flask.render_template('index.html')
+
+@app.rout("/data-send", methods=['POST'])
+def file_upload():
+    file = request.data
+
+    original_text        = speechToText.predict(file)
+    llama_corrected_text = llamaFrisian.predict(original_text)
+
+    return {
+        "original_text": orignal_text,
+        "llama_corrected_text": llama_corrected_text,
+    }
